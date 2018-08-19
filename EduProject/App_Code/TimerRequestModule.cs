6887dac77f7ac115cbe41cc81a5b5819c07090c5
Valery Čadovič <valerachad03@gmail.com>
2018-08-19 19:27:19 +0300

    Добавьте файлы проекта.

---
+++ EduProject/App_Code/TimerRequestModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace EduProject
+{
+    public class TimerRequestModule : IHttpModule
+    {
+        private Stopwatch timer;
+
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += this.OnBeginRequest;
+            context.EndRequest += this.OnEndRequest;
+        }
+
+        #endregion
+
+        private void OnBeginRequest(object sender, EventArgs ea)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        private void OnEndRequest(object sender, EventArgs ea)
+        {
+            var context = HttpContext.Current;
+            context.Response.Write($"<div style='color:red;'>Время обработки запроса: " +
+                                   $"{((float)timer.ElapsedTicks) / Stopwatch.Frequency:F5} секунд</div>");
+        }
+    }
+}
---
+++ EduProject/App_Code/VHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace EduProject
+{
+    public class VHandler : IHttpHandler
+    {
+        #region IHttpHandler Members
+
+        public bool IsReusable => false; 
+
+        public void ProcessRequest(HttpContext context)
+        {
+            StringBuilder sb = new StringBuilder(32);
+
+            foreach (string index in context.Request.Headers.Keys)
+            {
+                sb.Append($"{index}: ");
+                sb.Append(context.Request.Headers[index]);
+                sb.Append("<br/>");
+            }
+
+            sb.Append("<br/>");
+            sb.Append("<h2>Content</h2>");
+
+            using (StreamReader sr = new StreamReader(context.Request.PhysicalApplicationPath + context.Request.FilePath))
+            {
+                sb.Append(sr.ReadToEnd());
+            }
+            context.Response.Write(sb.ToString());
+        }
+        
+        #endregion
+    }
+}
